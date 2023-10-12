990dbbbff058563e9471147c54b7f9323f94a3a5
Md-arefin <ansariarefin617@gmail.com>
2023-10-12 15:13:53 +0600

    implemented dlt  cart

--- index.js
+++ index.js
@@ -196,12 +196,6 @@ async function run() {
         });
 
         // Cart related API
-        app.post('/carts', async(req, res) =>{
-            const items = req.body;
-            const result = await cartsCollection.insertOne(items);
-            res.send(result);
-        })
-
         app.get('/carts/:email', async(req, res) =>{
             const email = req.params.email;
             if(!email){
@@ -211,8 +205,21 @@ async function run() {
             const result = await cartsCollection.find(query).toArray();
             res.send(result);
         })
+       
+        
+        app.post('/carts', async(req, res) =>{
+            const items = req.body;
+            const result = await cartsCollection.insertOne(items);
+            res.send(result);
+        })
 
-        // 
+        app.delete("/carts/:id", async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result= await cartsCollection.deleteOne(query);
+            res.send(result)
+        })
+    
 
         // JWT related api
         app.post("/jwt", async (req, res) => {
